1fabf18fcd581cfb7d69077f26a2014c38ab387f
AndrWalker <117857483+AndrWalker@users.noreply.github.com>
2024-02-18 18:37:47 +0300

    Add files

---
+++ OrderService.cs
@@ -0,0 +1,40 @@
+﻿using TestTask.Services.Interfaces;
+using TestTask.Data;
+using TestTask.Models;
+using System.Collections.Generic;
+
+namespace TestTask
+{
+
+    public class OrderService : IOrderService
+    {
+        private ApplicationDbContext dataBase;
+        public OrderService(ApplicationDbContext dataBase)
+        {
+            this.dataBase = dataBase;
+        }
+
+        public Task<Order> GetOrder()
+        {
+            var highCostOrder = (from order in this.dataBase.Orders
+                         orderby (order.Quantity * order.Price) descending
+                         select order).FirstOrDefault();
+            
+            return Task.FromResult(highCostOrder);
+        }
+
+        public Task<List<Order>> GetOrders()
+        {
+            var quantityMore10Order = (from order in this.dataBase.Orders
+                                 where order.Quantity > 10
+                                 select order).ToList();
+            foreach (var order in quantityMore10Order)
+                order.User = (from user in this.dataBase.Users
+                               where user.Id == order.UserId
+                               select user).First();
+
+            return Task.FromResult(quantityMore10Order);
+        }
+    }
+
+}
---
+++ UserService.cs
@@ -0,0 +1,60 @@
+﻿using TestTask.Services.Interfaces;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Enums;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+namespace TestTask
+{
+
+    public class UserService : IUserService
+    {
+        private ApplicationDbContext dataBase;
+        public UserService(ApplicationDbContext dataBase)
+        {
+            this.dataBase = dataBase;
+        }
+
+        public Task<User> GetUser() {
+
+            User largestOrderUser = null;
+
+            var largestOrder = (from order in this.dataBase.Orders
+                         group order by order.UserId into Users
+                         orderby Users.Count() descending
+                         select Users.FirstOrDefault()).ToList();
+
+            if (largestOrder.Count != 0)
+            {
+
+                largestOrderUser = (from user in this.dataBase.Users
+                                    where user.Id == largestOrder.First().UserId
+                                    select user).FirstOrDefault();
+                largestOrderUser.Orders = largestOrder;
+            }
+            else
+            {
+                largestOrderUser = (from user in this.dataBase.Users 
+                                    select user).FirstOrDefault();
+            }
+            
+            return Task.FromResult(largestOrderUser);
+        }
+
+        public Task<List<User>> GetUsers()
+        {
+            var inactiveUsers = (from user in this.dataBase.Users
+                         where user.Status == UserStatus.Inactive
+                         select user).ToList();
+            foreach (var user in inactiveUsers)
+            {
+                var orders = (from order in this.dataBase.Orders
+                              where order.UserId == user.Id
+                              select order).ToList();
+                user.Orders = orders;
+            }
+            return Task.FromResult(inactiveUsers);
+        }
+    }
+
+}
